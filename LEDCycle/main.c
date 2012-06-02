4dfabe47ffc5117a2854d62321a1728bf03c149e
Psyphormer <Psyphormer@Psyphormer-PC>
2012-06-02 23:31:09 +0100

    Refactored code

--- LEDCycle/main.c
+++ LEDCycle/main.c
@@ -3,10 +3,9 @@
 #define LED_PORTJ (BIT0 | BIT1 | BIT2 | BIT3)
 #define LED_PORT3 (BIT4 | BIT5 | BIT6 | BIT7)
 
-void init_ports()
+
+void cycle()
 {
-	P3DIR |= LED_PORT3;
-	PJDIR |= LED_PORTJ;
 
 	P3OUT |= LED_PORT3;
 	PJOUT |= LED_PORTJ;
@@ -19,27 +18,31 @@ void init_ports()
 			P3OUT <<= 0x01;
 			//Reset
 			if (!(PJOUT && P3OUT)) {
-				//PJOUT |= BIT3;
-				//P3OUT |= BIT7;
 				while (!(PJOUT & 0x01) && !(P3OUT & 0x01)) {
 					if (!(PJOUT && P3OUT)) {
 						PJOUT |= BIT3;
-						P3OUT |= BIT7;
+							P3OUT |= BIT7;
 					}
 					PJOUT >>= 0x01;
 					P3OUT <<= 0x01;
 					__delay_cycles(30000);
 				}
-
-				PJOUT |= 0xFF;
-				P3OUT |= 0xFF;
+					PJOUT |= 0xFF;
+			P3OUT |= 0xFF;
 			}
 		}
 		__delay_cycles(30000);
 	}
 }
 
+void init_ports()
+{
+	P3DIR |= LED_PORT3;
+	PJDIR |= LED_PORTJ;
+}
+
 void main(void) {
 	WDTCTL = WDTPW + WDTHOLD; //Stop watchdog timer
 	init_ports();
+	cycle();
 }
